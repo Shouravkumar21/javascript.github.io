6d8979a962531a3b50670a1b0f1b77fb096d484e
Shouravkumar21 <93762547+Shouravkumar21@users.noreply.github.com>
2024-01-21 10:55:46 +0000

    conversion

--- .devcontainer/one/convesion.js
+++ .devcontainer/one/convesion.js
@@ -16,3 +16,40 @@ console.log(typeof (valueInNumber));
 
 //"true"->1
 //"false"->0
+
+
+////operatio////
+
+
+let value=9;
+let negValue=-value;
+console.log(negValue);
+
+
+//(2+2)=4
+//(2**2)=2^2
+//(2*3)=6
+
+
+let Sam="shourav";
+
+console.log(Sam.toUpperCase());
+console.log(Sam.length)
+console.log(Sam.charAt(3));
+console.log(Sam.indexOf("u"));
+
+//slice
+let anotherStr=(Sam.slice(1,3))
+console.log(anotherStr);
+
+
+//to remove extra space
+
+let Hitesh="    bkbdo kndandnlnl lknpdsndedd/    ";
+console.log(Hitesh);
+console.log(Hitesh.trim());
+
+
+let url="https//bjkdsbv/bjkbfsfb/20//vm";
+console.log(url.replace("20","-"));
+console.log(url.split("-"));
