a23a13df99597f2ffeb7f8fb241f52463c0637c8
Shouravkumar21 <93762547+Shouravkumar21@users.noreply.github.com>
2024-01-21 09:13:56 +0000

    Datatyoe

---
+++ .devcontainer/one/Datatype.js
@@ -0,0 +1,12 @@
+"use strict";//treat all js code as newer version
+
+//alert(3 + 3); //we use node.js not broswer so it is not working//
+
+//number
+//string ->""
+//boolean ->true/false
+//symbol
+//null->standalone value
+//undefind -> ex)var sam;
+
+//object
---
+++ .devcontainer/one/variable.js
@@ -0,0 +1,17 @@
+var AccountId="beohokfopdnjk";
+let panNumber="ywepruophifbk";
+const bankName="kjdkhlcbj";
+sam=".bcnc/;";
+
+
+
+AccountId="sakj,bdk";
+panNumber=".kbcj,b.,";
+
+
+//bankName="dk,b,jbckl/d";  --in cont we did not change its vale//
+
+sam=",cdbcbl,";
+
+
+console.table([panNumber,bankName,sam]);
